882847951323f97a581bf8b5a097a4f790d4167e
agarciarin <albertogarciarin97@gmail.com>
2022-10-11 23:43:55 +0200

    venv_2.0

--- sources/milestone2/Milestone_2.py
+++ sources/milestone2/Milestone_2.py
@@ -1,42 +1,48 @@
 #main
-import numpy as np
 import matplotlib.pyplot as plt
-import Functions as fun
-import Orbits as orb
+import numpy as np
+
 import CauchyProblem as cp
+import Functions as fun
 import Methods as met
+import Orbits as orb
 
-#Initiate values
-print("Introduzca dt = ")
-dt = float(input())
-#dt = 0.01       #[s] 
-tf = 30.        #[s]
+def main():
 
-R0 = 1.         #[m]
-V0 = 1.         #[m/s]
-Zeta0 = 0.      #[deg]
+    #Initiate values
+    print("Introduzca dt = ")
+    dt = float(input())
+    #dt = 0.01       #[s] 
+    tf = 30.        #[s]
 
-N = int(tf/dt)+1    #Nº steps
-t = np.linspace(0, tf, N)
-U0 = fun.InitStateVector(R0, V0, Zeta0)
+    R0 = 1.         #[m]
+    V0 = 1.         #[m/s]
+    Zeta0 = 0.      #[deg]
 
-#Cauchy solver orbits
-U_Euler = cp.CauchyProblem(U0, t, orb.Orbits, met.ExplicitEuler)
-U_RK4 = cp.CauchyProblem(U0, t, orb.Orbits, met.RungeKutta4)
-U_InvEuler = cp.CauchyProblem(U0, t, orb.Orbits, met.InverseEuler)
-U_CN = cp.CauchyProblem(U0, t, orb.Orbits, met.CrankNicolson)
-UU = np.array([U_Euler, U_RK4, U_InvEuler, U_CN])
+    N = int(tf/dt)+1    #Nº steps
+    t = np.linspace(0, tf, N)
+    U0 = fun.InitStateVector(R0, V0, Zeta0)
 
-#Energy orbit
-E_Euler = orb.OrbitEnergy(U_Euler, t)
-E_RK4 = orb.OrbitEnergy(U_RK4, t)
-E_InvEuler = orb.OrbitEnergy(U_InvEuler, t)
-E_CN = orb.OrbitEnergy(U_CN, t)
-E = np.array([E_Euler, E_RK4, E_InvEuler, E_CN])
+    #Cauchy solver orbits
+    U_Euler = cp.CauchyProblem(U0, t, orb.Orbits, met.ExplicitEuler)
+    U_RK4 = cp.CauchyProblem(U0, t, orb.Orbits, met.RungeKutta4)
+    U_InvEuler = cp.CauchyProblem(U0, t, orb.Orbits, met.InverseEuler)
+    U_CN = cp.CauchyProblem(U0, t, orb.Orbits, met.CrankNicolson)
+    UU = np.array([U_Euler, U_RK4, U_InvEuler, U_CN])
 
-Met_names = np.array(["Euler", "RK4", "Inverse Euler", "CN"])
+    #Energy orbit
+    E_Euler = orb.OrbitEnergy(U_Euler, t)
+    E_RK4 = orb.OrbitEnergy(U_RK4, t)
+    E_InvEuler = orb.OrbitEnergy(U_InvEuler, t)
+    E_CN = orb.OrbitEnergy(U_CN, t)
+    E = np.array([E_Euler, E_RK4, E_InvEuler, E_CN])
 
-#Graphics
-fun.plot_Positions(UU, Met_names, dt)
-fun.plot_energy(E, Met_names, t)
-plt.show()
+    Met_names = np.array(["Euler", "RK4", "Inverse Euler", "CN"])
+
+    #Graphics
+    fun.plot_Positions(UU, Met_names, dt)
+    fun.plot_energy(E, Met_names, t)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
