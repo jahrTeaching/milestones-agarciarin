00b38e40c60fbc518de11efb72aa5527826a9e9b
agarciarin <albertogarciarin97@gmail.com>
2022-10-23 20:16:54 +0200

    milestone3

---
+++ sources/milestone3/fun/cauchy_problem.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def cauchy_problem(U0, t, f, method):
+    U = np.array(np.zeros([len(U0), len(t)]))
+    U[:,0] = np.transpose(U0)
+    for i in range(len(t)-1):
+        dt = t[i+1] - t[i]
+        U[:,i+1] = method(U[:,i], dt, t, f)
+
+    return U
---
+++ sources/milestone3/fun/convergence_rate.py
@@ -0,0 +1,27 @@
+import numpy as np
+import fun.cauchy_problem as cp
+
+
+def convergence_rate(U0, t, f, method, m): #(U0, time, function, method, nº points)
+    log_E = np.zeros(m)
+    log_N = np.zeros(m)
+    N = len(t)-1
+    tf = t[N-1]
+
+    t1 = t
+    U1 = cp.cauchy_problem(U0, t1, f, method)
+
+    for i in range(m):
+        N = 2*N
+        t2 = np.linspace(0, tf, N+1)
+        U2 = cp.cauchy_problem(U0, t2, f, method)
+
+        E = np.linalg.norm(U2[:,N] - U1[:,int(N/2)])
+        log_E[i] = np.log10(E)
+        log_N[i] = np.log10(N)
+
+        t1 = t2
+        U1 = U2
+
+    return [log_N, log_E]
+    
